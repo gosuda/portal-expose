7a7caf7c84933baf9afa2cfe6af80bfaf016288d
kalee <splkm123@naver.com>
2025-11-21 12:36:56 +0900

    chore: update e2e test project image to ghcr.io/gosuda/portal-expose-controller:0.1.0

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -42,7 +42,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/portal-expose:v0.0.1"
+	projectImage = "ghcr.io/gosuda/portal-expose-controller:0.1.0"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
